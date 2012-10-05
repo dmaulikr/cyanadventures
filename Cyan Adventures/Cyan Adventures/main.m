5e468235b9cbe89df252f57087b460e71ae35670
Michael Paul <Michael@(null).(none)>
2012-10-05 16:19:46 +0100

    Initial Commit

---
+++ Cyan Adventures/Cyan Adventures/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cyan Adventures
+//
+//  Created by Michael Paul on 05/10/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
